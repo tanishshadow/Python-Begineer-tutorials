8a1e0464d28d93e9823f59e6466f45ce3bba24c7
tanishshadow <70163670+tanishshadow@users.noreply.github.com>
2020-08-29 15:04:36 +0530

    Create Join in python.py

---
+++ Join in python.py
@@ -0,0 +1,8 @@
+lst=["Tanish","Manish","Abhinandan","Arnab"]
+# for item in lst:
+#     print(item,"and",end=" ")
+# join
+a=" and ".join(lst)
+print(a)
+# print("They are my friends")
+# join also removes extra and as in case of for loops.
